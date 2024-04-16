cc936e5febfc721de2c0b53d63c18195077b0d3c
the-albertox <126242013+The-Albertox@users.noreply.github.com>
2024-04-16 12:17:04 +0100

    feat(Menu): create menu class

--- src/net/salesianos/menu/Menu.java
+++ src/net/salesianos/menu/Menu.java
@@ -1,5 +1,16 @@
 package net.salesianos.menu;
 
 public class Menu {
-    
+    public String ShowMenu() {
+        String Menu = """
+                Menu
+                1.Añiadir restaurente.
+                2.Editar restaurente.
+                3. Mostrar restaurente.
+                4.Eliminar restaurente.
+                Q. salir.
+                """;
+        System.out.println(Menu);
+        return Menu;
+    }
 }
