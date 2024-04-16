ad911208e893101a779f0dabab76ae8fcc7397d2
the-albertox <126242013+The-Albertox@users.noreply.github.com>
2024-04-16 11:52:44 +0100

    fix(Restaurante): change list to main

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
+import net.salesianos.clases.Restaurante;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<Restaurante> restaurants = new ArrayList<>();
+        Restaurante juan = new Restaurante("casaJuan", "sitio", "si", 4.5f);
+        restaurants.add(juan);
+        
     }
 }
