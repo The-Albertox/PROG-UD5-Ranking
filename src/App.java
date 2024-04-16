df974840a69ab9b9ae922758ab10b8a5f620d12a
the-albertox <126242013+The-Albertox@users.noreply.github.com>
2024-04-16 13:54:53 +0100

    feat(Menu): add funtion aniadirRestaurante

--- src/App.java
+++ src/App.java
@@ -1,12 +1,50 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 import net.salesianos.clases.Restaurante;
+import net.salesianos.menu.Menu;
 
 public class App {
+    static final Scanner KEYBOARD = new Scanner(System.in);
+    static final ArrayList<Restaurante> RESTAURANTS = new ArrayList<>();
+
+
     public static void main(String[] args) throws Exception {
-        ArrayList<Restaurante> restaurants = new ArrayList<>();
-        Restaurante juan = new Restaurante("casaJuan", "sitio", "si", 4.5f);
-        restaurants.add(juan);
+
+        boolean salir = true;
+        String option = "";
+
+        Menu menu = new Menu();
+        while (salir) {
+            option = menu.requestMenuOption();
+            switch (option) {
+                case "1":
+                    aniadirRestaurante();
+                    break;
+                case"2":
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    public static void aniadirRestaurante() {
+
+        String name = JOptionPane.showInputDialog(null, "introduzca el nombre ");
+
+        String localization = JOptionPane.showInputDialog(null, "introduzca la localizcacion");
+
+        String eschedual = JOptionPane.showInputDialog(null, "introduzca el hotario");
+
+        String puntuacionText = JOptionPane.showInputDialog(null, "introduzca la puntuacion", null);
+        float puntation = Float.parseFloat(puntuacionText);
+
+        Restaurante restaurante = new Restaurante(name, localization, eschedual, puntation);
+        RESTAURANTS.add(restaurante);
+
         
     }
 }
